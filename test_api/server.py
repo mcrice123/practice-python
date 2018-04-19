a7bd8b2597f2b6bf9519e2ab057fdb745373ce4b
Maria Rice <mcrice@shockers.wichita.edu>
2018-04-18 23:05:29 -0500

    Add practice resources and methods to server

--- test_api/server.py
+++ test_api/server.py
@@ -36,13 +36,40 @@ class Artists(Resource):
     def get(self):
         conn = db_connect.connect() 
         query = conn.execute("select * from artists")
-        return {'artists': [i[0] for i in query.cursor.fetchall()]}
+        return {'artists': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         
+class Albums(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select * from albums")
+        return {'albums': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+    def post(self, artist_id, title):
+        conn = db_connect.connect()
+        query = conn.execute("insert into albums values (=%d, =%s) " %int(artist_id) %str(title))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Artist(Resource):
+    def get(self, artist_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from artists where ArtistId =%d "  %int(artist_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Album(Resource):
+    def get(self, album_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from albums where AlbumId =%d "  %int(album_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
 api.add_resource(Employees, '/employees') # Route_1
 api.add_resource(Tracks, '/tracks') # Route_2
 api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 api.add_resource(Artists, '/artists')
+api.add_resource(Albums, '/albums')
+api.add_resource(Artist, '/artists/<artist_id>')
+api.add_resource(Album, '/albums/<album_id>')
 
 
 if __name__ == '__main__':
